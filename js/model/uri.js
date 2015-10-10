bc7677d582b829753f5e8e0b706c444586add3bb
Marcos Silva <chuckymass@gmail.com>
2015-10-10 12:51:52 -0300

    Estrutura base para pegar os dados do uri

--- js/model/uri.js
+++ js/model/uri.js
@@ -1,8 +1,21 @@
 var URI = (function(){
-	_public = {};
+
 	_private = {};
-	_public.ola = function(){
-		console.log("ola");
+	_private.baseUrl = "https://www.urionlinejudge.com.br/";
+	_private.judgePath = "judge/";
+	_private.profilePath = "profile/"; 
+	_private.defaultLang = 'pt';
+	_private.getUserById = function(id){
+		console.log(id);
+		console.log(_private);
 	};
+
+	_public = {};
+	_public.userId = "";
+	_public.getUser = _private.getUserById;
+	_public.get = function(){
+		_public.getUser(123);
+	}
+
 	return _public;
 })();
