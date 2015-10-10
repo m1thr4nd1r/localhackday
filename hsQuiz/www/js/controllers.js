cb3fa1706acab78bf2b05500c19e2dc4295c97ed
Marcos Silva <chuckymass@gmail.com>
2015-10-10 17:10:14 -0300

    Flip effect

--- hsQuiz/www/js/controllers.js
+++ hsQuiz/www/js/controllers.js
@@ -3,6 +3,7 @@ angular.module('starter.controllers', ['ionic-material','ngAnimate','ionMdInput'
 
 .controller('AppCtrl', function ($scope, $stateParams, HearthStoneService) {
     $scope.title = "Hearth Stone Quiz";
+    $(".classic-card").flip();
     // var data = HearthStoneService.query();
     // data.then(function(result){  // this is only run after $http completes
     //     $scope.provas = result.response;
